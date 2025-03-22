a67bafac840e5dbdb5eb1881fe4387d4994296e8
Aman Singh <amansingh09122004@gmail.com>
2025-03-22 16:53:36 +0530

    Top App bar created

--- lib/repository/screens/cart/cartscreen.dart
+++ lib/repository/screens/cart/cartscreen.dart
@@ -1,13 +1,91 @@
+import 'package:blinkit_clone/repository/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
 
 class Cartscreen extends StatelessWidget {
-  const Cartscreen({super.key});
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("CartScreen"),
+      body: Column(
+        children: [
+          SizedBox(height: 25),
+          Stack(
+            children: [
+              Container(
+                height: 160,
+                width: double.infinity,
+                color: Color(0XFFF7CB45),
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        SizedBox(width: 20),
+                        Uihelper.customText(
+                          text: "Blinkit in",
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.bold,
+                          fontsize: 15,
+                          fontfamily: "bold",
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        SizedBox(width: 20),
+                        Uihelper.customText(
+                          text: "16 Minutes",
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.bold,
+                          fontsize: 20,
+                          fontfamily: "bold",
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        SizedBox(width: 20),
+                        Uihelper.customText(
+                          text: "HOME - ",
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.bold,
+                          fontsize: 12,
+                        ),
+                        Uihelper.customText(
+                          text: "Sujal Dave, Ratanada, Jodhpur (Raj)",
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.normal,
+                          fontsize: 12,
+                        ),
+                        SizedBox(width: 5),
+                        Icon(Icons.arrow_drop_down_sharp),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              Positioned(
+                right: 20,
+                bottom: 80,
+                child: CircleAvatar(
+                  backgroundColor: Color(0XFFFFFFFF),
+                  radius: 20,
+                  child: Icon(Icons.person),
+                ),
+              ),
+
+              Positioned(
+                left: 20,
+                bottom: 20,
+                child: Uihelper.CustomTextField(controller: searchController),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/repository/widgets/uihelper.dart
+++ lib/repository/widgets/uihelper.dart
@@ -22,4 +22,25 @@ class Uihelper {
       ),
     );
   }
+
+  static CustomTextField({required TextEditingController controller}) {
+    return Container(
+      height: 37,
+      width: 380,
+      decoration: BoxDecoration(
+        color: Color(0XFFFFFFFF),
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Color(0XFFC5C5C5)),
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: "Search 'ice-creams'",
+          prefixIcon: Icon(Icons.search_rounded),
+          suffixIcon: Icon(Icons.mic),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
 }
