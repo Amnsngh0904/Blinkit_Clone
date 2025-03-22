69c6b2e8437f4b19d91c968fef4eff44aad2e010
Aman Singh <amansingh09122004@gmail.com>
2025-03-22 12:59:33 +0530

    login screen completed

--- lib/repository/screens/login/loginscreen.dart
+++ lib/repository/screens/login/loginscreen.dart
@@ -1,16 +1,118 @@
 import 'package:blinkit_clone/repository/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
 
-class Loginscreen extends StatelessWidget {
-
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Uihelper.CustomImage(img: "Blinkit Onboarding Screen.png")
+            Uihelper.customImage(img: "Blinkit Onboarding Screen.png"),
+
+            SizedBox(height: 30),
+
+            Uihelper.customImage(img: "logo.png"),
+
+            SizedBox(height: 20),
+
+            Uihelper.customText(
+              text: "India's last minute app",
+              color: Color(0XFF000000),
+              fontweight: FontWeight.w700,
+              fontsize: 24,
+              fontfamily: "bold",
+            ),
+
+            SizedBox(height: 20),
+
+            Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                height: 200,
+                width: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0XFFFFFFFF),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+
+                    Uihelper.customText(
+                      text: "Aman Singh",
+                      color: Color(0XFF000000),
+                      fontweight: FontWeight.w500,
+                      fontsize: 15,
+                      fontfamily: "regular",
+                    ),
+
+                    SizedBox(height: 5),
+
+                    Uihelper.customText(
+                      text: "80745XXXXX",
+                      color: Color(0XFF9C9C9C),
+                      fontweight: FontWeight.w700,
+                      fontsize: 14,
+                      fontfamily: "bold",
+                    ),
+
+                    SizedBox(height: 20),
+
+                    SizedBox(
+                      height: 48,
+                      width: 295,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0XFFE23744),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Uihelper.customText(
+                              text: "Login With",
+                              color: Color(0XFFFFFFFF),
+                              fontweight: FontWeight.w700,
+                              fontsize: 15,
+                              fontfamily: "bold",
+                            ),
+
+                            SizedBox(width: 5),
+                            Uihelper.customImage(img: "zomato logo.png"),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 5),
+
+                    Uihelper.customText(
+                      text:
+                          "Access your saved addresses from Zomato automatically!",
+                      color: Color(0XFF9C9C9C),
+                      fontweight: FontWeight.w600,
+                      fontsize: 10,
+                    ),
+
+                    SizedBox(height: 20),
+
+                    Uihelper.customText(
+                      text: "or login with phone number",
+                      color: Color(0XFF269237),
+                      fontweight: FontWeight.w600,
+                      fontsize: 14,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
--- lib/repository/screens/splash/splashscreen.dart
+++ lib/repository/screens/splash/splashscreen.dart
@@ -1,6 +1,8 @@
 import 'package:blinkit_clone/domain/constants/appcolors.dart';
 import 'package:blinkit_clone/repository/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:blinkit_clone/repository/screens/login/loginscreen.dart';
 
 class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
@@ -11,15 +13,24 @@ class Splashscreen extends StatefulWidget {
 
 class _SplashScreenState extends State<Splashscreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen())
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Appcolors.scaffoldbackground,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Uihelper.CustomImage(img: "image 1.png")
-          ],
+          children: [Uihelper.customImage(img: "image 1.png")],
         ),
       ),
     );
